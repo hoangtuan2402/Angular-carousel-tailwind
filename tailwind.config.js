b621a13fc38d3c4187ef41ea36ec5f44659ec228
Anhht_is <anhht@is.viettel.com.vn>
2022-05-31 17:14:30 +0700

    Responsive Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     darkMode: 'class',
     theme: {
         extend: {},
-
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'ipad': '800px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px'
+        },
 
     },
     plugins: [
