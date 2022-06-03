5a47f7604b0094770dbdb967b3110a4c8b8430b9
Shiro Angel <hoangtuananh.ub.tq@gmail.com>
2022-06-03 17:42:00 +0700

    Update Scrollable Chapter lít

--- src/app/list-chapter/list-chapter.component.ts
+++ src/app/list-chapter/list-chapter.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-chapter.component.css']
 })
 export class ListChapterComponent implements OnInit {
+   listChapter = [
+    {id: 1, name:'chapter1'},
+    {id: 2, name:'chapter2'},
+    {id: 5, name:'chapter3'},
+    {id: 3, name:'chapter4'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+    {id: 4, name:'chapter5'},
+];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.listChapter[1000];
   }
 
 }
