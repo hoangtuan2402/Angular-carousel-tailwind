8af6828d511c16b11bad04cd41443035fbdaaf3b
Shiro Angel <hoangtuananh.ub.tq@gmail.com>
2022-06-27 22:13:57 +0700

    Update Selected Tag val

--- src/app/publish/comics/comics.component.ts
+++ src/app/publish/comics/comics.component.ts
@@ -3,28 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comics',
   templateUrl: './comics.component.html',
-  styleUrls: ['./comics.component.css']
+  styleUrls: ['./comics.component.css'],
 })
 export class ComicsComponent implements OnInit {
   cars = [
-    { id: 1, name: "BMW Hyundai" },
-    { id: 2, name: "Kia Tata" },
-    { id: 3, name: "Volkswagen Ford" },
-    { id: 4, name: "Renault Audi" },
-    { id: 5, name: "Mercedes Benz Skoda" },
+    { id: 1, name: 'BMW Hyundai' },
+    { id: 2, name: 'Kia Tata' },
+    { id: 3, name: 'Volkswagen Ford' },
+    { id: 4, name: 'Renault Audi' },
+    { id: 5, name: 'Mercedes Benz Skoda' },
   ];
 
   tags = [
-    { id: 1, name: "Manhua" },
-    { id: 2, name: "Manga" },
-    { id: 3, name: "English" }
+    { id: 1, name: 'Manhua' },
+    { id: 2, name: 'Manga' },
+    { id: 3, name: 'English' },
   ];
-  selected = [{ id: 3, name: "Volkswagen Ford" }];
-  selectedTags = [{ id: 1, name: "Manhua" }];
+  selected = [{ id: 3, name: 'Volkswagen Ford' }];
+  selectedTags = [{ id: 1, name: 'Manhua' }];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeSelected(val: any) {
+    console.log(val);
   }
-
 }
