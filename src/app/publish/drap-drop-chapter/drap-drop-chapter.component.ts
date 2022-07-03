f3979e316cc4f4856ddceb666e04d4d67c276dec
Shiro Angel <hoangtuananh.ub.tq@gmail.com>
2022-07-03 12:32:38 +0700

    https://stackblitz.com/edit/angular-2bem3j?file=src%2Fapp%2Fapp.component.html

--- src/app/publish/drap-drop-chapter/drap-drop-chapter.component.ts
+++ src/app/publish/drap-drop-chapter/drap-drop-chapter.component.ts
@@ -9,6 +9,19 @@ import { UserService } from '../user.service';
 })
 export class DrapDropChapterComponent implements OnInit {
 users: any;
+indexOver: number = -1;
+
+  icons = [
+    'https://picsum.photos/100/100?random=1',
+    'https://picsum.photos/100/100?random=2',
+    'https://picsum.photos/100/100?random=3',
+    'https://picsum.photos/100/100?random=4',
+    'https://picsum.photos/100/100?random=5'
+    
+  ];
+  cells = Array(25)
+  .fill(' ')
+  .map((_, index) => ({ src: null, id: index }));
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
@@ -16,8 +29,23 @@ users: any;
       this.users = users;
     });
   }
-  drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
-    moveItemInArray(this.users, event.previousIndex, event.currentIndex);
+  drop(event:CdkDragDrop<any>) {
+    if (event.previousContainer != event.container) {
+      if (event.container.data.src !== undefined) { //we are drop on "board"
+        if (event.previousContainer.data.src != undefined) { //we are dragging an element of "board"
+          event.container.data.src = event.previousContainer.data.src;
+          event.previousContainer.data.src = null;
+        } else {  //we are dragging an element of "side"
+          event.container.data.src = event.previousContainer.data;
+        }
+      } else {
+        if (
+          event.container.data.src === undefined &&
+          event.previousContainer.data.src !== undefined
+        ) //we are drop an img from "board" on the "side"
+          event.previousContainer.data.src = null;
+      }
+    }
   }
 
 
