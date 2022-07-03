32929207fa0189e776eaf3e1f67820f4eb724cdc
Shiro Angel <hoangtuananh.ub.tq@gmail.com>
2022-07-03 16:59:50 +0700

    Update Drag And Drop

--- src/app/publish/drap-drop-chapter/drap-drop-chapter.component.ts
+++ src/app/publish/drap-drop-chapter/drap-drop-chapter.component.ts
@@ -11,17 +11,7 @@ export class DrapDropChapterComponent implements OnInit {
 users: any;
 indexOver: number = -1;
 
-  icons = [
-    'https://picsum.photos/100/100?random=1',
-    'https://picsum.photos/100/100?random=2',
-    'https://picsum.photos/100/100?random=3',
-    'https://picsum.photos/100/100?random=4',
-    'https://picsum.photos/100/100?random=5'
-    
-  ];
-  cells = Array(25)
-  .fill(' ')
-  .map((_, index) => ({ src: null, id: index }));
+
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
@@ -30,24 +20,17 @@ indexOver: number = -1;
     });
   }
   drop(event:CdkDragDrop<any>) {
+    console.log("Drag Event");
+    
     if (event.previousContainer != event.container) {
-      if (event.container.data.src !== undefined) { //we are drop on "board"
-        if (event.previousContainer.data.src != undefined) { //we are dragging an element of "board"
-          event.container.data.src = event.previousContainer.data.src;
-          event.previousContainer.data.src = null;
-        } else {  //we are dragging an element of "side"
-          event.container.data.src = event.previousContainer.data;
-        }
-      } else {
-        if (
-          event.container.data.src === undefined &&
-          event.previousContainer.data.src !== undefined
-        ) //we are drop an img from "board" on the "side"
-          event.previousContainer.data.src = null;
-      }
+        
+          let temp = event.container.data.name.first;
+          event.container.data.name.first = event.previousContainer.data.name.first;
+          event.previousContainer.data.name.first = temp;
+          console.log(temp);
+          
     }
   }
-
 
   submitList(){
       console.log(this.users);
